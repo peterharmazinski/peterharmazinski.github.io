c70f68c554efa1d961a7ee0426aa6b49d353b44a
Peter Harmazinski <pharmazinski@gmail.com>
2018-12-23 16:14:05 -0600

    stop navbar from hiding on scroll

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-window.onscroll = function(e) { 
-  var scrollY = window.pageYOffset || document.documentElement.scrollTop;
-  var header = document.querySelector('header');
-
-  scrollY <= this.lastScroll 
-    ? header.style.visibility = 'visible'
-    : header.style.visibility = 'hidden'; 
-
-  this.lastScroll = scrollY ;
-}
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
